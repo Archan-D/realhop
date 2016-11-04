d29ae45baa66694cf9dae0e2ce34a4551c24ddbc
Archan Dave <archandave101@gmail.com>
2016-11-04 10:24:50 -0400

    hi

--- Hop/app/src/main/java/com/example/shred13/hop/Play.java
+++ Hop/app/src/main/java/com/example/shred13/hop/Play.java
@@ -26,7 +26,8 @@ int counter=0;
             ImageView circle = (ImageView) findViewById(R.id.imageView);
             if (counter =% 1) {
                 circle.setBackgroundColor(Color.BLUE);
-                //counter++;
+                counter++;
+                //some random comment
 
             }
         }
